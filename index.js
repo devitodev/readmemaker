4ec63a59eb79bdc3dcec8f971356ad55b4a2394a
devitodev <softwaredev.ito@gmail.com>
2022-12-22 18:34:14 -0500

    Added all functionality and worked out the project

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project\'s title?',
+      },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description for your project?',
+      },
+    {
+        type: 'input',
+        name: 'dependencies',
+        message: 'What command should be run to install your project\'s dependencies'
+      },
+    {
+        type: 'input',
+        name: 'utility',
+        message: 'How is your app used?'
+      },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'What command should be used to run tests?'
+      },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'How would you like users to contribute to this repo?'
+      },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your github username?'
+      },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+      },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of license does your project have?',
+        choices: ['MIT','Apache 2.0','GPL 3.0','BSD 3','None']
+      },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(),'/dist',fileName),data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((responses) => {
+    writeToFile('README.md', generateMarkdown(responses))
+    console.log('It worked!')
+    })
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,59 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return ` ![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `* [License](#license)`;
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License \n this project is licensed under the ${license} license`;
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.title} 
+  ${renderLicenseBadge(data.license)}
+  ## Description: 
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  ${renderLicenseLink(data.license)}
+  * [Questions](#questions)
+  ## Installation 
+  ${data.dependencies}
+  ## Usage 
+  ${data.utility}
+  ## Contribution
+  ${data.contribute}
+  ${renderLicenseSection(data.license)}
+  ## Tests
+  ${data.testing}
+  ## Questions
+  If you have any questions you can email me at ${data.email}\n
+  You can see more of my work at [${data.username}](https://github.com/${data.username})`
+  ;
 }
 
 module.exports = generateMarkdown;
